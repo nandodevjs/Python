7ac11a7d9eb9b68f359d5c1ecd5f49e380afacdb
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2022-06-07 21:29:49 -0300

    Comentários em Python

---
+++ logica_de_Programacao/comentarios_em_Python.py
@@ -0,0 +1,11 @@
+# Comentários no Python se inicia com #
+
+"""
+Comentários de mutiplas linhas com três aspas duplas, são bastante usados para documentação
+"""
+
+'''
+Também é possível comentar com três aspas simples
+'''
+
+print('Helo World')
