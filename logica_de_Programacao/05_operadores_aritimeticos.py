469d629c1cde5eebe672d50dc8e8920f6e75108f
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2022-06-10 16:38:57 -0300

    Operadores aritméticos
    - Trabalhando com operadores aritméticos
    
    - Soma
    - Subtração
    - Divisão
    - Potenciação

---
+++ logica_de_Programacao/05_operadores_aritimeticos.py
@@ -0,0 +1,22 @@
+'''
++ = soma
+
+- = Subtração
+
+* = Mutiplicação
+
+/ = Divisão
+
+// = Divisão inteira
+
+% = Módulo (resto da divisão)
+
+'''
+
+print('Soma:', 10 + 10)
+print('Subtração:', 10 - 10)
+print('Mutiplicação:', 10 * 10)
+print('Potenciação:', 10 ** 10)
+print('Divisão:', 10 / 10)
+print('Divisão inteira:', 10 // 10)
+print('Módulo/Resto:', 10 % 10)
